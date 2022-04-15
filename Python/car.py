50e55bb1bd4d2c55b9516a7b1aa1b898378ad9f1
Ing. Luis Almazán <luis.14xbox@live.com.mx>
2022-04-14 20:37:55 -0600

    polimorfismo java y php 2

--- Python/car.py
+++ Python/car.py
@@ -2,4 +2,21 @@ class Car:
      id=int
      license=str
      driver=str
-     passenger=int
+     _passenger=int
+
+     def __init__(self,license,driver):
+         self.license=license
+         self.driver=driver
+         self._passenger=int
+
+     def get_passenger(self):
+	     return self._passenger
+
+     def set_passenger(self, passenger):
+	     if passenger < 0:
+		     raise ValueError("Uber debe tener 4 asientos")
+
+	     self._passenger = passenger
+
+     def printDataCar(self):
+          pass
